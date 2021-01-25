ecb86156c2680396a2b0bcec55013a641f8d9093
Darshil <61809014+darshilagrawal@users.noreply.github.com>
2021-01-25 15:51:37 +0530

    A Basic Display app for Images

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by Darshil Agrawal on 13/03/20.
+//  Copyright © 2020 Darshil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
